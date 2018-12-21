a8a1e8bdc76a6ada2bbeac781944a545e2f7bc98
niklasengvall <niklas_engvall@hotmail.com>
2018-12-21 19:39:27 +0100

    6:e bearbetningen

---
+++ Kodexempel/Concat - Kap. 3.3, sid. 57.cs
@@ -0,0 +1,25 @@
+// Concat.cs
+// Ritar en hjärtlig hälsning
+// Anropar metoden Write() utan radbyte
+// Slår ihop strängar med konkateneringsoperatorn +
+
+using System;
+
+class Concat
+{
+    static void Main()
+    {
+        Console.Write("\n\t     *       *            " +
+                      "\n\t   *   *   *   *          " +
+                      "\n\t *       *       *        " +
+                      "\n\t*                 *       " +
+                      "\n\t*                 *       " +
+                      "\n\t*     Grattis     *       " +
+                      "\n\t *               *        " +
+                      "\n\t   *           *          " +
+                      "\n\t     *       *            " +
+                      "\n\t       *   *              " +
+                      "\n\t         *                " +
+                      "\n\t");
+    }
+}
