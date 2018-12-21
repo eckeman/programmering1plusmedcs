f50a9faf78f232a01a22e66ab1bf64caf660b7d4
niklasengvall <niklas_engvall@hotmail.com>
2018-12-21 16:04:51 +0100

    4: bearbetningen

---
+++ Kodexempel/LineBreak.cs
@@ -0,0 +1,12 @@
+﻿// LineBreak.cs
+// Skriver ut text med radbyte och indragning
+
+using System;
+
+class LineBreak
+{
+    static void Main()
+    {
+        Console.WriteLine("\n\tC#\n\tär\n\tkul !\n");
+    }
+}
---
+++ Kodexempel/Output.cs
@@ -0,0 +1,14 @@
+// Output.cs
+// Två utskriftsmetoder, men endast EN utskriftsrad
+// Metoden Write() skriver uttext utan radbyte efteråt
+
+using System;
+
+class Output
+{
+    static void Main()
+    {
+        Console.Write("\n\tDetta är En rad text produc");
+        Console.WriteLine("erad av två utskriftsatser");
+    }
+}
